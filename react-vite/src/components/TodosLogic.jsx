cdf5956e37a33b14519e7bf5477d8fe6f88ef026
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-03-01 22:00:43 +0000

    Remove hard coded state in todo logic

--- react-vite/src/components/TodosLogic.jsx
+++ react-vite/src/components/TodosLogic.jsx
@@ -5,23 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 
 
 const TodosLogic = () => {
-    const [todos, setTodos] = useState([
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ]);
+    const [todos, setTodos] = useState([]);
     const handleChange = (id) => {
         setTodos((prevState) =>
         prevState.map((todo) => {
