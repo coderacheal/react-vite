bc10959e8e15361422efd57685ec69a7d27bc39e
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-03-01 23:18:12 +0000

    complete router process

---
+++ react-vite/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'about', text: 'About' },
+  { path: 'profile', text: 'Profile' },
+  { path: 'login', text: 'Login' },
+];
+
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+    <ul>
+      {links.map((link) => {
+        return (
+          <li key={link.text}>
+            <NavLink to={link.path}
+             cstyle={({ isActive }) => ({
+              color: isActive ? 'red' : undefined,
+            })}>
+        {link.text}
+    </NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  </nav>
+    )
+  };
+  export default Navbar;
+  
--- react-vite/src/components/TodoApp.jsx
+++ react-vite/src/components/TodoApp.jsx
@@ -2,16 +2,38 @@ import Header from "./Header";
 import TodosLogic from "./TodosLogic";
 
 
+// const TodoApp = () => {
+//     return (
+//       // <div className="wrapper">
+//       //   <div className="todos">
+//       //     <Header />
+//       //     <TodosLogic />
+//       //   </div>
+//       // </div>
+//     );
+//   };
+  
+// export default TodoApp;
+
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from '../routes/Home';
+import About from '../routes/About';
+import Login from '../routes/Login';
+import Profile from '../routes/Profile';
+import NotMatch from '../routes/NotMatch';
+
 const TodoApp = () => {
-    return (
-      <div className="wrapper">
-        <div className="todos">
-          <Header />
-          <TodosLogic />
-        </div>
-      </div>
-    );
-  };
-  
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
+  );
+};
 export default TodoApp;
-  
